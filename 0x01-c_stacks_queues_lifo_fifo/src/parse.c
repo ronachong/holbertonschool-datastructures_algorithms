a7f661a0011dd149a9fe452a9be964566a4c951a
Rona Chong <ronachong@gmail.com>
2017-06-11 18:26:55 -0700

    Begin implementing parse with fgets to parse monty input.

--- 0x01-c_stacks_queues_lifo_fifo/src/parse.c
+++ 0x01-c_stacks_queues_lifo_fifo/src/parse.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define NCHARS 1024
 
 int parse(FILE *fp)
 {
+  char buff[NCHARS];
+
+  printf("in parse, using %p\n", (void *) fp);
+  while (fgets (buff, NCHARS, fp))
+    {
+      printf("%s", buff);
+      printf("\n\n");
+    }
+  return (0);
 }

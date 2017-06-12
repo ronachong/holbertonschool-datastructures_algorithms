6104bee00aa1fc886c6b97d252adf310211a1e01
Rona Chong <ronachong@gmail.com>
2017-06-11 21:51:18 -0700

    partially implement checks to call push/pall ops.

--- 0x01-c_stacks_queues_lifo_fifo/src/parse.c
+++ 0x01-c_stacks_queues_lifo_fifo/src/parse.c
@@ -8,6 +8,7 @@ int parse(FILE *fp)
   char lbuff[NCHARS];
   char wbuff[NCHARS];
   int lnum;
+  /* int i; */
 
   printf("in parse, using %p\n", (void *) fp);
   for (lnum = 1; fgets (lbuff, NCHARS, fp); lnum++)
@@ -15,13 +16,24 @@ int parse(FILE *fp)
       printf("%s", lbuff);
       sscanf(lbuff, "%s", wbuff);
 
+      /* printf("iterating thru wbuff till null char:\n"); */
+      /* for (i = 0; wbuff[i] != '\0'; i++) */
+      /* 	{ */
+      /* 	  putchar(wbuff[i]); */
+      /* 	} */
+      /* printf("\n\n"); */
+
       if (strcmp(wbuff, "push") == 0)
       	{
-      	  ;
+      	  sscanf(lbuff, "%s", wbuff);
+	  printf("would pass %s to push()\n", wbuff);
+	  /* push(wbuff); */
       	}
       else if (strcmp(wbuff, "pall") == 0)
       	{
-      	  ;
+      	  sscanf(lbuff, "%s", wbuff);
+	  printf("would pass %s to pall()\n", wbuff);
+	  /* pall(wbuff); */
       	}
       else
       	{

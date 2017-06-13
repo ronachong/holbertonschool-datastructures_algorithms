ba537ced4491bf3e0187209b200e8726894e7a6b
Rona Chong <ronachong@gmail.com>
2017-06-13 16:17:16 -0700

    First implementation of func to do binary search of array.

--- 0x01-big-0-binary-search/search_algos.h
+++ 0x01-big-0-binary-search/search_algos.h
@@ -2,5 +2,7 @@
 #define HEADER_FILE
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_search(int *array, int start, int end);
 
 #endif

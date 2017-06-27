eedeb0594e3be265deda8687fb8a486a1731b854
Rona Chong <ronachong@gmail.com>
2017-06-27 14:46:07 -0700

    Begin implementation of jump search.

---
+++ 0x02-more_search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - jump search an array of given size for the specified value
+ * @array - pointer to first element in array
+ * @size - number of elements in array
+ * @value - value to search for in array
+ *
+ * Return: first index where @value is located; else -1 for failure to find @value or if array is null.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+}
--- 0x02-more_search_algorithms/search_algos.h
+++ 0x02-more_search_algorithms/search_algos.h
@@ -1,4 +1,7 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
 
+int jump_search(int *array, size_t size, int value);
+
+
 #endif

d6497dcd5e97838d66bdb4135209b126e7ab0b7f
Rona Chong <ronachong@gmail.com>
2017-06-27 15:14:59 -0700

    First iteration of jump search.

--- 0x01-big-0-binary-search/README.md
+++
@@ -1,3 +0,0 @@
-There are two solutions in this directory (0-linear.c for a linear search implementation and 1-binary.c for a binary search implementation).
-
-The rest of the files in this directory are time or space complexity assessments for various algorithms.
--- 0x02-more_search_algorithms/100-jump.c
+++ 0x02-more_search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
  * jump_search - jump search an array of given size for the specified value
@@ -10,4 +11,33 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+	unsigned int i;
+	unsigned int j;
+	unsigned int k; /* jump step size */
+	unsigned int l;
+
+	k = sqrt(size);
+
+	/* jump */
+	for (i = k, j = 0; i < size && array; i += k)
+	{
+		if (array[i] > value)
+		{
+			printf("Value found between indexes [%i] and [%i]\n", j, i);
+			/* check values */
+			for (l = j; l < i; l++)
+			{
+				printf("Value checked array[%i] = [%i]\n", l, array[l]);
+				if (array[l] == value)
+					return (l);
+			}
+
+			/* no matching value found: */
+			break;
+		}
+		else if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }

6d505cc8491e3ff784623cb2784f1fcd6c52aed6
Rona Chong <ronachong@gmail.com>
2017-06-29 17:43:04 -0700

    Begin implementing exponential search function.

---
+++ 0x02-more_search_algorithms/103-exponential.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - search array for value using exponential search
+ * @array: pointer to first element in array being searched
+ * @size: number of elements in array
+ * @value: value to search for in array
+ *
+ * Return: first index where @value is present in @array, or else -1 if value
+ * is not present in array or array is null.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+}

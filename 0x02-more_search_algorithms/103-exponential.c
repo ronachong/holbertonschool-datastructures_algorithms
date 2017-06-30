c49c586637fc33fa2107fa2271f866c8b13838c2
Rona Chong <ronachong@gmail.com>
2017-06-29 18:29:09 -0700

    Implement exponential bound part of exponential search.

--- 0x02-more_search_algorithms/103-exponential.c
+++ 0x02-more_search_algorithms/103-exponential.c
@@ -11,4 +11,23 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
+	unsigned int u_bound;
+	unsigned int l_bound;
+	
+	if (array == NULL || array[size - 1] < value)
+		return (-1);
+
+	u_bound = 1;
+	while (u_bound < size && array[u_bound] < value)
+	{
+		printf("Value checked array[%i] = [%i]\n", u_bound, array[u_bound]);
+		u_bound *= 2;
+	}
+
+	l_bound = u_bound/2;
+	u_bound = (u_bound < size) ? u_bound:size - 1;
+	printf("Value found between indexes [%i] and [%i]\n", l_bound, u_bound);
+	printf("Should do search now.\n");
+
+	return (-1);
 }

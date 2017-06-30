5fe7a2b422c9cdc97b1f33ca089446982f4765d2
Rona Chong <ronachong@gmail.com>
2017-06-29 20:21:03 -0700

    Optimize algo for binary search a little.

--- 0x01-big-0-binary-search/1-binary.c
+++ 0x01-big-0-binary-search/1-binary.c
@@ -15,31 +15,18 @@ int binary_search(int *array, size_t size, int value)
 	int start;
 	int end;
 
-	if (array == NULL)
-		return (-1);
-
-	i = size/2;
 	start = 0;
-	end = size;
-	while (i > -1 && (size_t) i < size)
+	end = size - 1;
+	while (start != end && array)
 	{
 		print_search(array, start, end);
+		i = start + (end - start)/2;
 		if (array[i] == value)
 			return (i);
 		if (array[i] > value)
-		{
-			start = 0;
-			end = i;
-			i = i/2;
-		}
+			end = i - 1;
 		else if (array[i] < value)
-		{
-			if (size - i == 1)
-				break; /* last ind in array has been checked*/
-			start = i;
-			end = size;
-			i += (size - i)/2;
-		}
+			start = i + 1;
 	}
 	return (-1);
 }
@@ -49,7 +36,7 @@ void print_search(int *array, int start, int end)
 	int j;
 
 	printf("Searching in array: ");
-	for (j = start; j < end - 1; j++)
+	for (j = start; j < end; j++)
 	{
 		printf("%i, ", array[j]);
 	}

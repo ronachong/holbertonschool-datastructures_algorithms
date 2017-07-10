9b9809655f1c1951f2b4ba14c34113bc20b504ca
Rona Chong <ronachong@gmail.com>
2017-07-09 21:37:43 -0700

    Add documentation to binary_tree_node.

--- 0x03-binary_trees/0-binary_tree_node.c
+++ 0x03-binary_trees/0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a new binary tree node
+ * @parent: pointer to parent node for new node
+ * @value: value for new node
+ *
+ * Return: pointer to new node, or NULL in case of malloc failure.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child;

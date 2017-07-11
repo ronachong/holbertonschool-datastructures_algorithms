1f89909d3f6732462458963de272ef3eccb9c347
Rona Chong <ronachong@gmail.com>
2017-07-11 13:11:20 -0700

    Add forgotten condition to cover case where node is NULL.

--- 0x03-binary_trees/4-binary_tree_is_leaf.c
+++ 0x03-binary_trees/4-binary_tree_is_leaf.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node->parent == NULL)
 		return 0;
 
-	if (node->left == NULL && node->right == NULL)
+	if (node && node->left == NULL && node->right == NULL)
 		return 1;
 
 	return 0;

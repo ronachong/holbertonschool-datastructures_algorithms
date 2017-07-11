15af1ce07e6094652704f599d5774891cd0479bb
Rona Chong <ronachong@gmail.com>
2017-07-11 15:15:02 -0700

    Fix documentation for traverse_in_order.

--- 0x03-binary_trees/7-binary_tree_inorder.c
+++ 0x03-binary_trees/7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - traverses a binary tree in pre-order fashion
+ * binary_tree_inorder - traverses a binary tree in order
  * @tree: pointer to root node of the tree to traverse
  * @func: pointer to a function to call for each node. Value i node will be passed as parameter to function.
  */
@@ -17,4 +17,3 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 
 
 }
- 

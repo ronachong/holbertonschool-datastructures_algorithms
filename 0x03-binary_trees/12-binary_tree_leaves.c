436781a2d123097c0c9807901e6a068f15902d86
Rona Chong <ronachong@gmail.com>
2017-07-12 12:38:42 -0700

    Implement function to find number of leaves in tree.

---
+++ 0x03-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - finds the number of leaves in a binary tree
+ * @tree: pointer to root node in binary tree
+ *
+ * Return: number of leaves in tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree->right && tree->left)
+		return (binary_tree_leaves(tree->right) +
+			binary_tree_leaves(tree->left));
+
+	if (tree->right)
+		return (binary_tree_leaves(tree->right));
+
+	if (tree->left)
+		return (binary_tree_leaves(tree->left));
+
+	return (1);
+}

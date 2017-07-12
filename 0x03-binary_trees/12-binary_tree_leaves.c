4c6ef2c1edabb7b02631305166aa624a3f9ee817
Rona Chong <ronachong@gmail.com>
2017-07-12 12:53:16 -0700

    Handle null case in function to find number of leaves in tree.

--- 0x03-binary_trees/12-binary_tree_leaves.c
+++ 0x03-binary_trees/12-binary_tree_leaves.c
@@ -8,6 +8,9 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	if (tree->right && tree->left)
 		return (binary_tree_leaves(tree->right) +
 			binary_tree_leaves(tree->left));
